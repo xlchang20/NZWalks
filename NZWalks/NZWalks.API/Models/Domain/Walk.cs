99c8aedfb91d439cd7cebf389bf2cb1454e348d3
xlchang <xlchang20@gmail.com>
2022-06-08 23:45:51 -0500

    End of Session 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,27 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+
+        public String Name { get; set; }
+
+
+        public double Length { get; set; }
+
+
+        public Guid RegionId { get; set; }
+
+
+        public Guid WalkDifficultyId { get; set; }
+
+
+        // Navigation Properties, A Walk have a many to one Region.
+        public Region Region { get; set; }
+
+
+        // Navigation Properties, A Walk have a many to one WalkDifficulty.
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
